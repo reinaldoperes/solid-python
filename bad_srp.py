b1ad301466a91cd6dfdc043a0c6f21459bf649f9
reinaldoperes <reinaldoporte@hotmail.com>
2022-03-15 21:05:03 -0300

    feat: bad code without using single responsability principle

---
+++ bad_srp.py
@@ -0,0 +1,26 @@
+from urllib import response
+import requests
+import json
+
+class ListRepositories():
+  API_BASE_URL = 'https://api.github.com'
+  
+  def __init__(self, user):
+    self.user = user
+    
+  def get_repos_by_user(self):
+    response = requests.get(f'{self.API_BASE_URL}/users/{self.user}/repos')
+    
+    if response.status_code == 200:
+      return {'status_code': 200, 'body': response.json()}
+    
+    return {'status_code': response.status_code, 'body': 'Error while getting repos'}
+  
+  def parse_response(self):
+    response = self.get_repos_by_user()
+    body = response['body']
+    if response['status_code'] == 200:
+      for i in range(len(body)):
+        print(f'{body[i]["id"]} - {body[i]["name"]} - {body[i]["stargazers_count"]}')
+    
+repos = ListRepositories('reinaldoperes').parse_response()
