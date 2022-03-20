0e416295e00eafb33a66806a616299e8f1a2a4e6
reinaldoperes <reinaldoporte@hotmail.com>
2022-03-20 18:28:31 -0300

    refactor: applying the O principle

--- repo/reports_generator.py
+++ repo/reports_generator.py
@@ -1,13 +1,5 @@
-from .reports.html_generator import HTMLGenerator
-from .reports.markdown_generator import MarkdownGenerator
-
 class ReportsGenerator():
   
   @classmethod
-  def build(cls, type, repos):
-    if type == 'HTML':
-      return HTMLGenerator.build(repos)
-    elif type == 'MARKDOWN':
-      return MarkdownGenerator.build(repos)
-    else:
-      return 'Invalid report type'
+  def build(cls, generator, repos):
+    return generator.build(repos)
