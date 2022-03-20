8d8bb6d1ee85dc1ddc5116aeeafa48917f852dfd
reinaldoperes <reinaldoporte@hotmail.com>
2022-03-20 18:28:31 -0300

    refactor: applying the O principle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from github.client import GithubClient
 from repo.parser import RepoParser
+from repo.reports.html_generator import HTMLGenerator
+from repo.reports.markdown_generator import MarkdownGenerator
 from repo.reports_generator import ReportsGenerator
 
 if __name__ == '__main__':
@@ -8,8 +10,8 @@ if __name__ == '__main__':
   
   if response['status_code'] == 200:
     repos = RepoParser.parse(response['body'])
-    markdown_report = ReportsGenerator.build('MARKDOWN', repos)
-    html_report = ReportsGenerator.build('HTML', repos)
+    markdown_report = ReportsGenerator.build(MarkdownGenerator, repos)
+    html_report = ReportsGenerator.build(HTMLGenerator, repos)
     
     print(markdown_report)
     print(html_report)
